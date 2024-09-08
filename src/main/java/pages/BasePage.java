2ab27d450149fa1f2ad035fca9090d33fa8e73ab
Olga Azarkh <olga.azarkh@gmail.com>
2024-09-08 15:01:45 +0300

    firstProject

---
+++ src/main/java/pages/BasePage.java
@@ -0,0 +1,13 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class BasePage {
+
+    static WebDriver driver;
+
+    public static void setDriver(WebDriver wd){
+        driver=wd;
+    }
+
+}
